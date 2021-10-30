3d7a431d629842bad45e1d36851c0fa1ef954be4
damuha <dyg_hm@yahoo.co.jp>
2021-10-30 01:30:03 +0000

    first commit

---
+++ pages/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+// import "./App.css";
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">TypeScriptはいいぞ</header>
+    </div>
+  );
+}
+ 
+export default App;
